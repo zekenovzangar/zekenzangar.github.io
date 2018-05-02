353f2cfdc7266abe5efa487b307c2c5b36354f65
zekenovzangar <38916809+zekenovzangar@users.noreply.github.com>
2018-05-02 20:29:19 +0600

    Новая версия 1,0
    загрузили 6 файлов

---
+++ script.js
@@ -0,0 +1,15 @@
+  /*var hello;
+  hello = 'Пока'
+  alert (hello)
+  var a='при',b='вет'
+alert (a+b)
+var isAdmin=confirm ('Вы Админ?')
+alert (isAdmin)*/
+ var year=prompt ('В каком году было первый ЭКСПО?');
+ if (year<2017){
+   alert ('Рано!');
+ }else if (year>2017){
+   alert ('поздно');
+ }else {
+   alert ('правельно');
+ }
